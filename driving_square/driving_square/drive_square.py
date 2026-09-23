7e801b162defda2759f417421bfd8bb8e339fe8f
detch <thanadetch.m@gmail.com>
2026-09-22 21:54:23 -0400

    Refactored to be working with timer

--- driving_square/driving_square/drive_square.py
+++ driving_square/driving_square/drive_square.py
@@ -1,5 +1,6 @@
 """ This script commands the neato to move in a square pattern.  """
-from time import sleep
+import time
+from time import sleep 
 import math
 import rclpy
 from rclpy.node import Node
@@ -11,49 +12,56 @@ class SquareDriver(Node):
         """ Initializes the SendMessageNode. No inputs."""
         super().__init__('square_driver')
         # Create a timer that fires ten times per second (10 Hz)
+        self.time_start = time.time()
+        self.actions = 8
+        self.count = 0
+
+        self.move_distance = 1
+        self.linear_velocity = 0.25
+        self.drive_time = self.move_distance/self.linear_velocity
+
+        self.angular_velocity = 0.2
+        self.rotate_angle = math.pi/2
+        self.rotate_time = self.rotate_angle /self.angular_velocity
+
         timer_period = 0.1
-        self.timer = self.create_timer(timer_period, self.run_square)
-        self.publish_velocity = self.create_publisher(msg_type=Twist, topic='cmd_vel', qos_profile=10)
+        self.create_timer = self.create_timer(timer_period, self.run_square)
+        self.pub_vel = self.create_publisher(msg_type=Twist, topic='cmd_vel', qos_profile=10)
 
     def run_square(self):
-        """ Moves it in a square."""
-        for _ in range(4):
-            # loop to move forward for 1 meter in a square
-            self.move_distance(1)
-            self.rotate()
-
-    def move_distance(self, distance:int=1):
-        """" Moves it in a set distance, with the default being 1"""
-        linear_velocity:float = 0.25
-        self.drive(linear=linear_velocity)
-        sleep(distance/linear_velocity)
-        self.drive(linear=0.0, angular=0.0)
-        print('Finished moving')
-
-    def drive(self, linear:float=0.25, angular:float=0.0):
-        """ Moves it with a set linear velocity"""
+        """Moves the neato in the square exactly once. This happens through four rotations and four movements.
+        By default the rotations are counter clockwise. It uses self.count and self.action to limit
+        the number of possible actions, and action_time to determine actions. When the action timer has
+        elapsed, it will always stop temporarily before going to the next action, and it resets the
+        action timer for the next action."""
         twist_msg = Twist()
-        twist_msg.linear.x = linear
-        twist_msg.angular.z = angular
-        self.publish_velocity.publish(twist_msg)
-
-    def rotate(self, angle:float=90): 
-        """Rotates the neato with a set angle, given in degrees
-        Input: angle (float), an angle to move the Neato by a set degrees"""
-        radian_angle:float = angle*math.pi/180
-        angular_velocity:float = 0.2
-        self.drive(linear=0.0, angular = angular_velocity)      
-        sleep(radian_angle/angular_velocity)               
-        self.drive(linear=0.0, angular = 0.0)
-        print('Finished rotating')
-        
+        action_time = time.time() - self.time_start
+        if self.count < self.actions:
+            if (action_time < self.drive_time) and (self.count % 2 == 0):
+                twist_msg.linear.x = self.linear_velocity
+                twist_msg.angular.z = 0.0
+                self.pub_vel.publish(twist_msg)
+            elif (action_time < self.rotate_time) and (self.count % 2 == 1):
+                twist_msg.linear.x = 0.0
+                twist_msg.angular.z = self.angular_velocity
+                self.pub_vel.publish(twist_msg) 
+            else:
+                self.count += 1
+                self.time_start = time.time()
+                twist_msg.linear.x = 0.0
+                twist_msg.angular.z = 0.0
+                self.pub_vel.publish(twist_msg)
+        else:
+            twist_msg.linear.x = 0.0
+            twist_msg.angular.z = 0.0
+            self.pub_vel.publish(twist_msg)
 
 def main(args=None):
     """ Initializes a node, runs it, and cleans up after termination.
     Input: args(list) -- list of arguments to pass into rclpy. Default None.
     """
     rclpy.init(args=args)      # Initialize communication with ROS
-    node = SquareDriver()   # Create our Node
+    node = SquareDriver()      # Create our Node
     rclpy.spin(node)           # Run the Node until ready to shutdown
     rclpy.shutdown()           # cleanup
 
